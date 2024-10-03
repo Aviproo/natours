2d14d52b70484348da476a97dcd48b33e062b63b
AVINASH KUMAR <Aviproo>
2024-10-03 13:39:08 +0530

    Creating routes and middleware

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
+app.use(express.json());
 
-//
+app.use((req, res, next) => {
+  console.log('Hello from thr middleware');
+  next();
+});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 app.listen('3000', () => {
   console.log('App running on port 3000');
---
+++ routes/tourRoutes.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: tours,
+  });
+};
+const getTour = (req, res) => {
+  console.log(req.params, tours.length);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tour,
+    },
+  });
+};
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1] + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+const upadateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Update tour here...',
+    },
+  });
+};
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: {
+      tour: null,
+    },
+  });
+};
+router.route('/').get(getAllTours).post(createTour);
+
+router.route('/:id').get(getTour).patch(upadateTour).delete(deleteTour);
+
+module.exports = router;
