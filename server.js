e85bb2ac7e86de457ed0553c9a693bf15539f599
AVINASH KUMAR <Aviproo>
2024-10-05 11:50:00 +0530

    creating all crud operation with database

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
-dotenv.config();
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
@@ -17,38 +17,6 @@ mongoose
     console.log('DB connection succesfully');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A toure must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR', err);
-  });
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
